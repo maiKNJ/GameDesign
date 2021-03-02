469e21fefe964a6f162e1c26fea40c00e90de9e0
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2021-03-02 14:42:00 +0100

    updates collision

---
+++ GameDesign/Assets/Script/DAD.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DAD : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject player;
+    float jumpSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpSpeed;
+        }
+        if (collision.gameObject.tag == "DAD")
+        {
+            collision.gameObject.GetComponent<Collider2D>().enabled = false;
+        }
+        else
+        {
+            collision.gameObject.GetComponent<Collider2D>().enabled = true;
+        }
+    }
+}
--- GameDesign/Assets/Script/walk.cs
+++ GameDesign/Assets/Script/walk.cs
@@ -7,11 +7,14 @@ public class walk : MonoBehaviour
     private Rigidbody2D rigidBody;
     public float jumpSpeed = 8f;
     public float speed = 2f;
+    bool slide;
+    public Collider2D obstacle;
     
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = transform.GetComponent<Rigidbody2D>();  
+        rigidBody = transform.GetComponent<Rigidbody2D>();
+        obstacle = obstacle.GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -23,18 +26,28 @@ public class walk : MonoBehaviour
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
 
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             rigidBody.velocity = Vector2.up * jumpSpeed;
         }
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) )
         {
-            gameObject.transform.Rotate(0, 0, 45, Space.Self);
-            
+            //gameObject.transform.Rotate(0, 0, 45, Space.Self);
+            slide = true;
+            obstacle.enabled = false;
+            Debug.Log("Collider.enabled = " + obstacle.enabled);
+
         }
         else if(Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
         {
-            gameObject.transform.Rotate(0, 0, -50, Space.Self);
+            //gameObject.transform.Rotate(0, 0, -50, Space.Self);
+            slide = false;
+            obstacle.enabled = true;
         }
     }
 }
