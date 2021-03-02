8973541c8a9e1339252dd3b7fae15b221d408c9c
maiKNJ <43847780+maiKNJ@users.noreply.github.com>
2021-03-02 12:41:02 +0100

    walk/jump

---
+++ GameDesign/Assets/Script/walk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float jumpSpeed = 8f;
+    public float speed = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = transform.GetComponent<Rigidbody2D>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.velocity = Vector2.up * jumpSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) )
+        {
+            gameObject.transform.Rotate(0, 0, 45, Space.Self);
+            
+        }
+        else if(Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            gameObject.transform.Rotate(0, 0, -50, Space.Self);
+        }
+    }
+}
